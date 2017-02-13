99de550ba487b19d9b7e75f219a483b55d29f217
praveen <praveen@OM69>
2017-02-13 11:35:45 +0530

    first check in

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        private static string format = "Hello {0},{1}{2}";
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your firstname :--");
+            string Name = Console.ReadLine();
+            Console.Write("Please enter your lastename :--");
+            string LName = Console.ReadLine();
+            Console.WriteLine(string.Format(format, Name, Environment.NewLine, Name + LName));
+            Console.Read();
+        }
+    }
+}
