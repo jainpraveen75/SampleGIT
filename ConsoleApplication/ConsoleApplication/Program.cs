9dd85ea0bea6f08572c5d2ecbde385f48977797f
jainpraveen75 <jainpraveen75@gmail.com>
2017-02-13 11:39:56 +0530

    added comments
    added comments

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -11,11 +11,15 @@ namespace ConsoleApplication
         private static string format = "Hello {0},{1}{2}";
         static void Main(string[] args)
         {
+            // get first Name
             Console.Write("Please enter your firstname :--");
             string Name = Console.ReadLine();
+            // get first last Name
             Console.Write("Please enter your lastename :--");
             string LName = Console.ReadLine();
+            // print all input 
             Console.WriteLine(string.Format(format, Name, Environment.NewLine, Name + LName));
+            // wait for next
             Console.Read();
         }
     }
